e7a376aac40eddd96d77b5610dd68f4a266e5f35
vijaykangude <vijaykangude.005@gmail.com>
2021-11-12 05:14:27 -0800

    Commented code Removed

--- application.js
+++ application.js
@@ -1,13 +1,10 @@
 // Simple Web App to explore get and post request by using Express framework
 const express = require("express");
-// const router = require("express").Router();
 const app = express();
 const port = 80;       // used a 80 port because we can view just typing localhost
 
-
 app.use('/' , require('./route'));
 
-// const fs = require("fs");   //file handling module imported for file read
 
 
 app.listen(port , () =>{
